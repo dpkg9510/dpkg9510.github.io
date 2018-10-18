93f97a0ec5d52125d58a93bebb30837eda244a67
dpkg_ <dpkg9510@gmail.com>
2018-10-18 11:16:52 +0200

    init commit

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-apt-ftparchive packages ./ > ./Packages
-sed -i -e '/^SHA/d' ./Packages
-bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: dpkg9510's Repo\nLabel: dpkg9510\nSuite: stable\nVersion: 1.0\nCodename: dpkg9510\nArchitecture: iphoneos-arm\nComponents: main\nDescription: dpkg9510's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
-exit 0
