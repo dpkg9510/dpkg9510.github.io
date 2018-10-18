d3edf33775eb4ce571d94c0390c41b0362ff7ad5
dpkg_ <dpkg9510@gmail.com>
2018-10-18 11:07:22 +0200

    init commit

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 apt-ftparchive packages ./ > ./Packages
 sed -i -e '/^SHA/d' ./Packages
 bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: julioverne's Repo\nLabel: julioverne\nSuite: stable\nVersion: 1.0\nCodename: julioverne\nArchitecture: iphoneos-arm\nComponents: main\nDescription: julioverne's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: dpkg9510's Repo\nLabel: dpkg9510\nSuite: stable\nVersion: 1.0\nCodename: dpkg9510\nArchitecture: iphoneos-arm\nComponents: main\nDescription: dpkg9510's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 exit 0
